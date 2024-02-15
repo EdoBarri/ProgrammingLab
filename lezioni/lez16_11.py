71c52c16aef0ea155393a714833cac5085634a97
EdoBarri <150350344+EdoBarri@users.noreply.github.com>
2024-02-15 19:17:05 +0000

    Pre esame

--- lezioni/lez16_11.py
+++
@@ -1,9 +0,0 @@
-def sum_list(my_list):
-    if my_list == []:
-        return (None)
-    else:
-        risultato=0
-        for item in my_list:
-            risultato=risultato+item
-        return (risultato)
-        
